ed715700e61e2be399768f853d6b14ff6346752a
Fursdon <Fursdon@2b5c4f5a-b9a3-11de-b52b-63f31fbada87>
2010-03-16 18:53:07 +0000

    Added new testing for a scatter plot from the GPS data -CTF

--- trunk/tests/HackneyFrameScribbles/src/DAS_GUI_Testing.java
+++ trunk/tests/HackneyFrameScribbles/src/DAS_GUI_Testing.java
@@ -1,5 +1,3 @@
-//This is a comment for bret.
-
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartFrame;
 import org.jfree.chart.ChartPanel;
@@ -10,20 +8,13 @@ import org.jfree.data.io.*;
 import org.jfree.layout.*;
 
 import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.event.MouseAdapter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
 import javax.swing.JFrame;
-import javax.swing.JInternalFrame;
 import javax.swing.JPanel;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-import javax.swing.JPopupMenu;
 
 
 public class DAS_GUI_Testing {
@@ -32,9 +23,9 @@ public class DAS_GUI_Testing {
 		XYSeries xAxisData = new XYSeries("X direction"), yAxisData = new XYSeries("Y direction"), zAxisData = new XYSeries("Z direction");
 		
 		try{
-			File csvFile = new File("output.csv");
+			File csvFile = new File("log_deadmans-curve.csv");
 			Scanner csvScanner = new Scanner(csvFile);
-			//csvScanner.nextLine();
+			csvScanner.nextLine();
 			Double timeInput = 0.0, lateralInput = 0.0, accelInput = 0.0;
 			
 			while(csvScanner.hasNextLine())
@@ -43,8 +34,6 @@ public class DAS_GUI_Testing {
 				timeInput = Double.valueOf(csvLine.nextToken(",").toString());
 				lateralInput = Double.valueOf(csvLine.nextToken(",").toString());
 				accelInput = Double.valueOf(csvLine.nextToken().toString());
-				//System.out.println("Line o' Data; Time: " + timeInput + ", Lat: " 
-				//		           + lateralInput + ", Accel: " + accelInput);
 				xAxisData.add(timeInput, lateralInput);
 				yAxisData.add(timeInput, accelInput);
 			}
@@ -67,85 +56,13 @@ public class DAS_GUI_Testing {
 		ChartPanel accelPanel = new ChartPanel(accelChart);
 		ChartPanel lateralPanel = new ChartPanel(lateralChart);
 		
-		JPanel jPanel1 = new JPanel(new BorderLayout());
-		JPanel jPanel2 = new JPanel(new BorderLayout());
-		jPanel1.add(accelPanel);
-		jPanel2.add(lateralPanel);
+		JPanel jframesPanel = new JPanel(new BorderLayout());
+		jframesPanel.add(accelPanel, BorderLayout.NORTH);
+		jframesPanel.add(lateralPanel, BorderLayout.SOUTH);
 		
-		JMenuBar menuBar = new JMenuBar();
-		JMenu menu1 = new JMenu("File");
-		JMenu menu2 = new JMenu("Edit");
-		JMenu menu3 = new JMenu("Help!");
-		JMenuItem menuHelp = new JMenuItem("No help yet....sorry!");
-		menu3.add(menuHelp);
-		menuBar.add(menu1);
-		menuBar.add(menu2);
-		menuBar.add(menu3);
-		
-		
-//		JPopupMenu popup = new JPopupMenu();
-//	    menuHelp.addActionListener(this);
-//	    popup.add(menuItem);
-//	    menuItem = new JMenuItem("Another popup menu item");
-//	    menuItem.addActionListener(this);
-//	    popup.add(menuItem);
-//
-//	    //Add listener to components that can bring up popup menus.
-//	    MouseListener popupListener = new PopupListener();
-//	    output.addMouseListener(popupListener);
-//	    menuBar.addMouseListener(popupListener);
-
-		Dimension interFrameSize = new Dimension(750,500);
-		Dimension outerFrameSize = new Dimension(1000,750);
-		
-		JFrame frameOuter;
-		JInternalFrame frameInnerTop, frameInnerBottom;
-		JPanel mainFramePanel = new JPanel(new BorderLayout());
-		frameOuter = new JFrame();
-		frameOuter.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frameInnerTop = new JInternalFrame("AccelChart",true,true,true,true);
-		frameInnerBottom = new JInternalFrame("LateralChart",true,true,true,true);
-		
-		frameInnerTop.setIconifiable(true);
-		frameInnerBottom.setIconifiable(true);
-		
-		frameInnerTop.add(jPanel1);
-		frameInnerBottom.add(jPanel2);
-		
-		frameInnerTop.setPreferredSize(interFrameSize);
-		frameInnerBottom.setPreferredSize(interFrameSize);
-		frameOuter.setSize(outerFrameSize);
-		
-		mainFramePanel.add(frameInnerTop);
-		mainFramePanel.add(frameInnerBottom);
-		frameOuter.setJMenuBar(menuBar);
-		frameOuter.add(mainFramePanel);
-		
-		frameInnerTop.pack();
-		frameInnerBottom.pack();
-		frameInnerTop.setVisible(true);
-		frameInnerBottom.setVisible(true);
-		
-		frameOuter.pack();
-		frameOuter.setVisible(true);
+		JFrame frame = new JFrame();
+		frame.add(jframesPanel);
+		frame.pack();
+		frame.setVisible(true);
 	}
 }
-
-/*class PopupListener extends MouseAdapter
-{
-	public void mousePressed(MouseEvent e) {
-        maybeShowPopup(e);
-    }
-
-    public void mouseReleased(MouseEvent e) {
-        maybeShowPopup(e);
-    }
-
-    private void maybeShowPopup(MouseEvent e) {
-        if (e.isPopupTrigger()) {
-            popup.show(e.getComponent(),
-                       e.getX(), e.getY());
-        }
-    }
-
-}*/
