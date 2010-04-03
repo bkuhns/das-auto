5261481999b31814225443342a798f0f5d8b3ac0
Fursdon <Fursdon@2b5c4f5a-b9a3-11de-b52b-63f31fbada87>
2010-04-03 05:11:00 +0000

    Added new classes handling race inputs, more fixing of the course drawing.
    TODO: Few fixes need for MapCourseDrawTest, and then proper methods added for GuiTableLayoutTest to view JFreeChart panels for the multiple types of acceleration.

---
+++ tests/HackneyFrameScribbles/src/AccelSample.java
@@ -0,0 +1,30 @@
+
+public class AccelSample extends LogSample{
+	
+	private int xValue, yValue, zValue;
+
+	public int getxValue() {
+		return xValue;
+	}
+
+	public void setxValue(int xValue) {
+		this.xValue = xValue;
+	}
+
+	public int getyValue() {
+		return yValue;
+	}
+
+	public void setyValue(int yValue) {
+		this.yValue = yValue;
+	}
+
+	public int getzValue() {
+		return zValue;
+	}
+
+	public void setzValue(int zValue) {
+		this.zValue = zValue;
+	}
+
+}
---
+++ tests/HackneyFrameScribbles/src/LogSample.java
@@ -0,0 +1,12 @@
+
+abstract public class LogSample {
+	private long timestamp;
+
+	public long getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(long timestamp) {
+		this.timestamp = timestamp;
+	}
+}
