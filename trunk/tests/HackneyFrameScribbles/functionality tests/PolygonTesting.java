1f1d41084e7a7c86c4c0c811b36ea3c3f098001f
Fursdon <Fursdon@2b5c4f5a-b9a3-11de-b52b-63f31fbada87>
2010-04-11 18:49:33 +0000

    Testing for polygon/Trapezoid, and for gridbaglayout.

---
+++ trunk/tests/HackneyFrameScribbles/functionality tests/PolygonTesting.java
@@ -0,0 +1,54 @@
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.GradientPaint;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Polygon;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.geom.Line2D;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+
+public class PolygonTesting extends JPanel {
+	
+	 public void paintComponent(Graphics g) {
+	    Graphics2D test2D = (Graphics2D)g;
+
+	    DasTrapezoid trape = new DasTrapezoid();
+	    trape.addPoint(100, 100);
+	    trape.addPoint(100, 150);
+	    trape.addPoint(100, 200);
+	    trape.addPoint(300, 300);
+	    trape.addPoint(300, 150);
+	    trape.addPoint(300, 0);
+	    
+	    Line2D testLine;
+	    
+	    //Color(int r, int g, int b) range: 0-255
+	    Color testRed = new Color(255,0,0);
+	    Color testGreen = new Color(0,200,0);
+	    test2D.drawPolygon(trape);
+	    GradientPaint gradient = new GradientPaint(100,150,testRed,300,150,testGreen,true);
+	    test2D.setPaint(gradient);
+	    test2D.fillPolygon(trape);
+	}
+	 
+	public static void main(String[] args) {
+	    JFrame frame = new JFrame();
+	    frame.setTitle("DrawPoly");
+	    frame.setSize(500, 500);
+	    frame.addWindowListener( new WindowAdapter(){ 
+	    							public void windowClosing(WindowEvent e) {
+	    								System.exit(0); 
+	    							} 
+	    						}
+	    						);
+	    Container contentPane = frame.getContentPane();
+	    contentPane.add(new PolygonTesting());
+
+	    frame.setVisible(true);
+	}
+}
