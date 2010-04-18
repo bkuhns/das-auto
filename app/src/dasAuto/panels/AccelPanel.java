d858c414df3158b76e01b27e156845fc1e5c49c6
Fursdon <Fursdon@2b5c4f5a-b9a3-11de-b52b-63f31fbada87>
2010-04-18 00:24:28 +0000

    Added functionality to AccelPanel to contain a ChartPanel and JFreeChart. TODO: Needs some work to fit correctly inside a Gridbag Layout pane.

--- app/src/dasAuto/panels/AccelPanel.java
+++ app/src/dasAuto/panels/AccelPanel.java
@@ -1,10 +1,53 @@
 package dasAuto.panels;
 
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.xy.*;
+import dasAuto.logData.samples.AccelSample;
+
 public class AccelPanel extends DataPanel {
 	
-	
-	public AccelPanel()
+	//int accelType determining x,y, or z axis with value of 0(x), 1(y), or 2(z)
+	public AccelPanel(int accelType)
 	{
+		XYSeries accelSeries = new XYSeries("accel series");
+		XYSeriesCollection accelSeriesCollection = new XYSeriesCollection();
+		XYDataset accelDataset;
 		
+		AccelSample currentSample;
+		int currentAccel;
+		
+		for(int accelParser=0; accelParser<accelFeed.size(); accelParser++)
+		{
+			currentSample = accelFeed.get(accelParser);
+			
+			if(accelType == 0)
+				currentAccel = currentSample.getxValue(); 
+			else if(accelType == 1)
+				currentAccel = currentSample.getyValue();
+			else if(accelType == 2)
+				currentAccel = currentSample.getzValue();
+			else {
+				//trouble: bad int for accelType, error handling here?
+				currentAccel = 0;
+			}
+			
+			if(currentAccel > 1000)
+				System.out.println("Time: " + currentSample.getTimestamp() + " , currentAccel: " + currentAccel);
+			
+			
+			accelSeries.add(currentSample.getTimestamp(),currentAccel);
+		}
+		
+		accelSeriesCollection.addSeries(accelSeries);
+		accelDataset = (XYDataset) accelSeriesCollection;
+		
+		JFreeChart accelFreeChart = ChartFactory.createXYLineChart("DAS Auto Chart: Accel/Time", "Time(ms)", "AccelerationG(unk-nown)", 
+				accelDataset, PlotOrientation.VERTICAL, false, false, false);
+		
+		ChartPanel accelChartPanel = new ChartPanel(accelFreeChart);
+		this.add(accelChartPanel);
 	}
 }
