de5bc0a796c3aa81b5b24b77c1c2b3e34c42d865
Fursdon <Fursdon@2b5c4f5a-b9a3-11de-b52b-63f31fbada87>
2010-04-18 00:29:42 +0000

    Added functionality to MultiPolyTesting, it now figures out a guess line length min & max, and the current line length for a given point, based upon lateral acceleration. TODO: in DasTrapezoid, need to add methods for determining slope and new points, and then an override on the Polygon draw (? something.) function.

--- tests/HackneyFrameScribbles/functionality tests/DasTrapezoid.java
+++ tests/HackneyFrameScribbles/functionality tests/DasTrapezoid.java
@@ -1,12 +1,18 @@
+import java.awt.Point;
 import java.awt.Polygon;
 
 
 public class DasTrapezoid extends Polygon {
+	
+	private Point	oldPointTrap1, oldPointTrap2, newPointTrap1, newPointTrap2,
+		 	oldPointYint, newPointYint;
+	private double  oldSlope, newSlope;
+	
 
-	public DasTrapezoid()
+	public DasTrapezoid(Point oldPoint,   Point newPoint, 
+						int currentWidth, int previousAccel, 
+						int currentAccel)
 	{
 		
 	}
-	
-	
 }
--- tests/HackneyFrameScribbles/functionality tests/MultiPolyTesting.java
+++ tests/HackneyFrameScribbles/functionality tests/MultiPolyTesting.java
@@ -1,8 +1,10 @@
 import java.awt.Container;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.awt.geom.Line2D;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -10,27 +12,112 @@ import javax.swing.JPanel;
 
 public class MultiPolyTesting extends JPanel {
 	
-	 Point point1 = new Point(100,100);
-	 Point point2 = new Point(200,200);
-	 Point point3 = new Point(250,250);
-	 Point point4 = new Point(300,250);
-	 double accel[] = {350, 500, 750, 1000};
-	 int minPolyWidth, maxPolyWidth;
+	public static Point[] arrayPoint = new Point[4];
+	private static final long serialVersionUID = 5369072467124325227L;
+	int minPolyWidth, maxPolyWidth, minAccel = 0, maxAccel = 0;
+	int accel[] = {375, 350, 995, 1000};
+	boolean firstTime = true;
 	
+	 
 	 public void paintComponent(Graphics g) {
+		 Graphics2D graphic = (Graphics2D)g;
+		 Point previousPoint = null, currentPoint = null;
+		 int currentWidth = 0, previousWidth = 0;
 		 
+		 if(firstTime) {
+			 calculateMinMaxPolyWidthAndMinMaxAccelValues();
+			 firstTime = false;		 
+		 }
 		 
+		//For the very first point(x,y), determine your line size.
+		 //This should be the current value ...as some proportion of min or max accel...Fuck.
+		 //So, Hokay. There should be a range of accel values from your log data.
+		 //maxAccel - minAccel: will give you this range. If you do a currentAccel - minAccel...
+		 //divided by this value; this will be the proportion of Width.
+		 //NOTE: Maybe we only need to calculate the maxPolyWidth we want, and just do portions
+		 //		 of that.
 		 
+		 for(int p=0; p<4; p++)
+		 { 
+			 currentPoint = arrayPoint[p];
+			 currentWidth = getCurrentWidth(p);
+			 
+			 /*  Check to see if we're on the first point to create a line for. If so...find the
+			  *  slope based upon the lateral acceleration.
+			  */
+			 if(previousPoint == null) {
+				 previousWidth = currentWidth;
+				 previousPoint = currentPoint;
+			 } else {
+				 
+				 
+				 previousPoint = currentPoint;
+			 }
+		 }
 	 }
 	 
 	 
-	 public void calculateMinMaxPolyWidth() {
+	 public void calculateMinMaxPolyWidthAndMinMaxAccelValues() {
+		 //For testing purposes, create a minimum width of 1% of the screen height (from pixels)
+		 //and a maximum width of 3% of the same.
+		 minPolyWidth = (int) Math.round(this.getHeight() * 0.01);
+		 maxPolyWidth = (int) Math.round(this.getHeight() * 0.03);
 		 
+		 for(int a=0; a<4; a++)
+		 {
+			 if(a == 0) {
+				 minAccel = accel[a];
+				 maxAccel = accel[a];
+			 } else {
+				 if(accel[a] < minAccel)
+					 minAccel = accel[a];
+				 if(accel[a] > maxAccel)
+					 maxAccel = accel[a];
+			 }
+				 
+		 }
+		 System.out.println("Min width: " + minPolyWidth + " , Max width: " + maxPolyWidth);
+		 System.out.println("Min accel: " + minAccel + " , Max accel: " + maxAccel);
+	 }
+	 
+	 
+	 public int getCurrentWidth(int currentNum) {
+		 int curWidth = 0;
+		 double currentWidthProportion = 
+				( (double)(accel[currentNum] - minAccel) / (double)(maxAccel - minAccel) );
+		 
+		 if(currentWidthProportion == 0.0)
+			 curWidth = minPolyWidth;
+		 else if (currentWidthProportion == 1.0)
+			 curWidth = maxPolyWidth;
+		 else
+		 {
+			 curWidth = minPolyWidth + 
+			 			    (int)Math.round(currentWidthProportion * (maxPolyWidth - minPolyWidth));
+		 }
+		 
+		 System.out.println("Current width Proportion: " + currentWidthProportion);
+		 System.out.println("Current width: " + curWidth);
+		 
+		 return curWidth;
+	 }
+	 
+	 
+	 public static void fillPoints() {
+		 for(int i = 0; i < 4; i++) {
+			 switch(i) {
+			 	case 0: arrayPoint[0] = new Point(100,100);
+			 	case 1: arrayPoint[1] = new Point(200,200);
+			 	case 2: arrayPoint[2] = new Point(250,250);
+			 	case 3: arrayPoint[3] = new Point(300,250);
+			 }
+		 }
 	 }
 	
-	
-	public static void main(String[] args) {
+	 
+	 public static void main(String[] args) {
 	    JFrame frame = new JFrame();
+	    fillPoints();
 	    frame.setTitle("MultiPoly");
 	    frame.setExtendedState(frame.getExtendedState() | JFrame.MAXIMIZED_BOTH);
 	    frame.addWindowListener( new WindowAdapter(){ 
@@ -40,8 +127,6 @@ public class MultiPolyTesting extends JPanel {
 	    						}
 	    						);
 	    Container contentPane = frame.getContentPane();
-	    
-	    
 	    contentPane.add(new MultiPolyTesting());
 	    frame.setVisible(true);
 	}
