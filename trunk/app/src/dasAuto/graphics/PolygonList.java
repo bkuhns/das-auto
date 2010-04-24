922d25e2907af235eb009f0e03181cfdc68cf3ce
Fursdon <Fursdon@2b5c4f5a-b9a3-11de-b52b-63f31fbada87>
2010-04-24 20:55:55 +0000

    Added an ArrayList class type of Polygons. Added mucho functionality to CourseMapPolygon.

---
+++ trunk/app/src/dasAuto/graphics/PolygonList.java
@@ -0,0 +1,8 @@
+package dasAuto.graphics;
+
+import java.util.ArrayList;
+
+public class PolygonList extends ArrayList<CourseMapPolygon> {
+	private static final long serialVersionUID = -1305250605021750963L;
+
+}
