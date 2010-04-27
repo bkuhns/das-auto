27cf7db2da72efc3d72f66971b3a1ca4d44ae798
bkuhns <bkuhns@2b5c4f5a-b9a3-11de-b52b-63f31fbada87>
2010-04-27 22:39:47 +0000

    Fixed calculation of timestamps to use SimpleDateFormat which now gives accurate timestamp values. Timestamps are also now relative to each run (the first timestamp is always equal to zero now). Added skeleton for Observer on CourseMapPanel to show Chip how it works.

--- trunk/app/src/dasAuto/LogFileReader.java
+++ trunk/app/src/dasAuto/LogFileReader.java
@@ -4,7 +4,9 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -19,7 +21,8 @@ public class LogFileReader {
 	private AccelFeed accelFeed = new AccelFeed();
 	private GpsFeed gpsFeed = new GpsFeed();
 	
-	private long lastTimestamp = 0;
+	private long previousTimestamp = 0;
+	private long firstTimestamp = 0;
 	
 	public final String SENSOR_GPS_ID = Messages.getString("LogFileReader.sensorGpsId");
 	public final String SENSOR_ACCEL_ID = Messages.getString("LogFileReader.sensorAccelId");
@@ -39,6 +42,10 @@ public class LogFileReader {
 			while(inputLog.ready()) {
 				String currentLine = inputLog.readLine();
 				
+				// TODO: We should be checking for a GPS line because it's messing
+				// up the accel feed. Basically we need to pretend that the GPS
+				// sample was there and increment the "previousTimestamp" value so
+				// the accelerometer deltas are computed properly.
 				if(isValidLine(currentLine)) {
 					String[] currentLineParts = currentLine.split(Messages.getString("LogFileReader.sensorSeparator"));
 					String[] sensorData = currentLineParts[1].split(Messages.getString("LogFileReader.dataSeparator"));
@@ -58,22 +65,25 @@ public class LogFileReader {
 		}
 	}
 	
+	
 	private void addGpsLine(String[] sensorData) {
-		Calendar calendar = Calendar.getInstance();
 		String utcTime = sensorData[0];
-		String utcDate = sensorData[7];
-		
-		Calendar year = Calendar.getInstance();
-		calendar.set(Integer.parseInt(utcDate.substring(4,6)) + year.get(Calendar.YEAR) / 1000 * 1000,
-				Integer.parseInt(utcDate.substring(2, 4)),
-				Integer.parseInt(utcDate.substring(0, 2)),
-				Integer.parseInt(utcTime.substring(0, 2)), 
-				Integer.parseInt(utcTime.substring(2, 4)),
-				Integer.parseInt(utcTime.substring(4, 6)));
-		lastTimestamp = calendar.getTimeInMillis() +  Long.parseLong(utcTime.substring(7, 10));
+		SimpleDateFormat sdf = new SimpleDateFormat("HHmmss.SSS");
+		try {
+			Date currentDateTime = sdf.parse(utcTime);
+			
+			if(firstTimestamp == 0) {
+				firstTimestamp = currentDateTime.getTime();
+				previousTimestamp = 0;
+			} else {
+				previousTimestamp = currentDateTime.getTime() - firstTimestamp;
+			}
+		} catch(Exception ex) {
+			System.err.println("Fuck");
+		}
 		
 		GpsSample gpsSample = new GpsSample();
-		gpsSample.setTimestamp(lastTimestamp);
+		gpsSample.setTimestamp(previousTimestamp);
 		gpsSample.setLatitude(Double.valueOf(sensorData[1]));
 		gpsSample.setLongitude(Double.valueOf(sensorData[3]));
 		gpsSample.setSpeed(Double.valueOf(sensorData[5]) * 1.15077945);
@@ -82,15 +92,17 @@ public class LogFileReader {
 		gpsFeed.add(gpsSample);
 	}
 	
+	
 	private void addAccelLine(String[] sensorData) {
 		AccelSample loadingAccel = new AccelSample();
-		loadingAccel.setTimestamp(lastTimestamp + Long.parseLong(sensorData[0]));
+		loadingAccel.setTimestamp(previousTimestamp + Long.parseLong(sensorData[0]));
 		loadingAccel.setXValue(1024 - Integer.parseInt(sensorData[1]));
 		loadingAccel.setYValue(Integer.parseInt(sensorData[2]));
 		loadingAccel.setZValue(Integer.parseInt(sensorData[3]));
 		
 		accelFeed.add(loadingAccel);
 	}
+	
 	
 	private boolean isValidLine(String currentLine) {
 		Pattern accelPattern = Pattern.compile("^\\d{2}?:\\d{1,},\\d{1,4},\\d{1,4},\\d{1,4}$");
@@ -110,6 +122,7 @@ public class LogFileReader {
 			return false;
 		}
 	}
+	
 	
 	public AccelFeed getAccelFeed() {
 		return accelFeed;
