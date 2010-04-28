c3c45d74c2eb98340aae953a076ec5b77faa648d
anthony.joseph.greer <anthony.joseph.greer@2b5c4f5a-b9a3-11de-b52b-63f31fbada87>
2010-04-28 00:17:40 +0000

    Added points to TC, will add arcs next time

--- app/src/dasAuto/panels/TractionCirclePanel.java
+++ app/src/dasAuto/panels/TractionCirclePanel.java
@@ -4,13 +4,19 @@ import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Point;
 import java.awt.image.BufferedImage;
 
 import javax.swing.BorderFactory;
 
+import dasAuto.logData.feeds.AccelFeed;
+
 
 public class TractionCirclePanel extends DataPanel {
 	private static final long serialVersionUID = -600064128610900053L;
+
+	private static final int MAX_CIRCLE_VAL_X = 5;
+	private static final int MAX_CIRCLE_VAL_Y = 5; 
 	
 	private int panelWidth;
 	private int panelHeight;
@@ -40,6 +46,7 @@ public class TractionCirclePanel extends DataPanel {
 		tracCircleG.fill(getVisibleRect());
 		
 		drawAxis(tracCircleG);
+		drawMaxCircle(tracCircleG);
 		
 		g2d.drawImage(tracCircleImage, 0, 0, this);
 	}
@@ -54,14 +61,52 @@ public class TractionCirclePanel extends DataPanel {
 	}
 	
 	
-	private void drawMaxCircle(Graphics2D g) {
-		//g.drawOval(accelFeed.getMinXValue(), y, width, height)
+	private void drawMaxCircle(Graphics2D g)
+	{
+		Point origin = this.getOrigin();
+		final AccelFeed accelFeed = this.accelFeed.getFilteredFeed(75);
+
+		//-- Draw Points first
+		this.drawPoint(origin.x + scaleX(accelFeed.getMinYValueInG()), origin.y, g); // Left
+		this.drawPoint(origin.x + scaleX(accelFeed.getMaxYValueInG()), origin.y, g); // Right
+		this.drawPoint(origin.x, origin.y + scaleY(accelFeed.getMaxXValueInG()), g); // Top 		
+		this.drawPoint(origin.x, origin.y + scaleY(accelFeed.getMinXValueInG()), g); // Bottom
+
+		//-- Then Arcs
+		//g.drawArc(origin.x,  origin.y + scaleY(accelFeed.getMaxXValueInG()), scaleX(accelFeed.getMaxYValueInG()), -scaleY(accelFeed.getMaxXValueInG()), 0, 90); // Top -> Right
+		//g.drawArc(0, (int)scaleY(accelFeed.getMaxXValueInG()), (int)scaleX(accelFeed.getMaxYValueInG()), (int)scaleY(accelFeed.getMaxXValueInG()), 90, 90);
+	}
+
+	private void drawPoint(int loc_x, int loc_y, Graphics2D g)
+	{
+		final int pointSize = 6;
+		g.drawOval(loc_x - (pointSize>> 1), loc_y - (pointSize >> 1), pointSize, pointSize);
+		g.fillOval(loc_x - (pointSize >> 1), loc_y - (pointSize >> 1), pointSize, pointSize);;
 	}
 	
 	
 	public void update(Graphics g) {
 		paint(g);
 	}
-	
-	
+
+	/*
+	 * Scale the accel coordinates to fit on the axis
+	 */
+	private int scaleX(double d)
+	{
+		return (int)Math.round(d * panelWidth / 2 / MAX_CIRCLE_VAL_X);
+	}
+
+	/*
+	 * Scale the accel coordinates to fit on the axis
+	 */
+	private int scaleY(double d)
+	{
+		return (int)Math.round(d * panelHeight / 2 / MAX_CIRCLE_VAL_Y);
+	}
+
+	private Point getOrigin()
+	{
+		return new Point(panelWidth / 2 - 1, panelHeight / 2 - 1);
+	}
 }
